b7dec602699de709c2a4b55e1fe9cafe3da467ba
陈清 <782223468@qq.com>
2019-04-28 15:24:25 +0800

    Update ViewController.swift
    测试提交

--- Example/CQLib/ViewController.swift
+++ Example/CQLib/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("11111")
+        
     }
 
     override func didReceiveMemoryWarning() {
